71ca2a6b690235f590911d6bfdf25f40a2faf4c7
vigih dev <vigihdev@gmail.com>
2025-12-13 03:35:35 +0700

    💥 2025-12-12 20:35:35

--- src/Post/List_Post_Command.php
+++ src/Post/List_Post_Command.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Vigihdev\WpCliEntityCommand\Post;
 
+use Vigihdev\Support\Collection;
 use Vigihdev\WpCliEntityCommand\WP_CLI\Post_Base_Command;
 use Vigihdev\WpCliModels\UI\CliStyle;
 use Vigihdev\WpCliModels\UI\Components\ListPreset;
 use WP_CLI;
+use WP_CLI\Iterators\Query;
 use WP_CLI_Command;
+use WP_Post;
+use WP_Query;
 
 final class List_Post_Command extends Post_Base_Command
 {
@@ -56,10 +60,58 @@ final class List_Post_Command extends Post_Base_Command
      */
     public function __invoke(array $args, array $assoc_args)
     {
+        $io = new CliStyle();
         WP_CLI::success(
             sprintf('Execute basic command from %s', Get_Post_Command::class)
         );
+
+        $query_args = [
+            'post_type'        => $assoc_args['post-type'] ?? 'post',
+            'post_status'      => $assoc_args['status'] ?? 'publish',
+            'posts_per_page'   => $assoc_args['limit'] ?? 15,
+            'offset'           => $assoc_args['offset'] ?? 0,
+            'orderby'          => $assoc_args['orderby'] ?? 'date',
+            'order'            => $assoc_args['order'] ?? 'DESC',
+            'category__in'     => isset($assoc_args['category']) ? explode(',', $assoc_args['category']) : [],
+            'author__in'       => isset($assoc_args['author']) ? explode(',', $assoc_args['author']) : [],
+            'post__in'         => isset($assoc_args['include']) ? explode(',', $assoc_args['include']) : [],
+            'post__not_in'     => isset($assoc_args['exclude']) ? explode(',', $assoc_args['exclude']) : [],
+        ];
+        $posts = get_posts($query_args);
+
+        if (!$posts) {
+            WP_CLI::warning('No posts found.');
+            return;
+        }
+
+
+
+        $this->process(
+            io: $io,
+            limit: $assoc_args['limit'] ?? 15,
+            offset: $assoc_args['offset'] ?? 0,
+            collection: new Collection(data: $posts),
+        );
     }
 
-    private function process() {}
+    /**
+     * Memproses data post dan menampilkan dalam bentuk tabel.
+     *
+     * @param int $limit      Jumlah post yang akan ditampilkan.
+     * @param int $offset     Jumlah post yang akan dilewati.
+     * @param Collection<WP_Post[]> $collection Koleksi data post.
+     */
+    private function process(CliStyle $io, int $limit, int $offset, Collection $collection)
+    {
+        $io->table(
+            fields: ['ID', 'Title', 'Status'],
+            items: $collection->map(function ($post) {
+                return [
+                    $post->ID,
+                    $post->post_title,
+                    $post->post_status,
+                ];
+            })->toArray(),
+        );
+    }
 }
